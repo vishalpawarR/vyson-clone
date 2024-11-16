4e5ee7f826f37b728d660fc4b6911b00657fead4
Vishal Pawar <vishalpawarr.git@gmail.com>
2024-11-16 12:09:44 +0530

    chore: changing icons

--- src/components/ui/Hero.jsx
+++ src/components/ui/Hero.jsx
@@ -1,39 +1,13 @@
 import Button from "./Button"
-import starSVG from "../../assets/hero-img/star.svg"
+import { CheckBadgeIcon } from "@heroicons/react/24/solid"
 
 function Hero() {
-  console.log(starSVG)
   return (
     <>
-      <div>
-        <svg
-          width="18"
-          height="19"
-          viewBox="0 0 18 19"
-          fill="none"
-          id="svg22051478_419"
-        >
-          <path
-            d="M9 0.0853577L10.8469 4.01102L14.7851 2.19096L13.6765 6.38536L17.8633 7.52252L14.318 10.0231L16.7942 13.5854L12.4711 13.222L12.0782 17.5426L9 14.4854L5.92182 17.5426L5.52895 13.222L1.20577 13.5854L3.68204 10.0231L0.13673 7.52252L4.32346 6.38536L3.21491 2.19096L7.15309 4.01102L9 0.0853577Z"
-            fill="#A9E8C2"
-          ></path>
-        </svg>
-
+      <div className="flex">
         <div>
-          <div>
-            <div
-              class="svgContainer"
-              style="width:100%;height:100%;aspect-ratio:inherit"
-            >
-              <svg
-                style="width:100%;height:100%"
-                viewBox="0 0 18 19"
-                preserveAspectRatio="none"
-              >
-                <use href="#svg22051478_419"></use>
-              </svg>
-            </div>
-
+          <div className="flex">
+            <CheckBadgeIcon className="w-5" />
             <p>Current cohort is full. Next cohort starts 15th November.</p>
           </div>
           <h1>
@@ -49,8 +23,8 @@ function Hero() {
         </div>
         <div>
           <img
-            src="../../assets/hero-img/hero-img.avif"
-            alt=""
+            src="/hero-img.avif"
+            alt="Hero image showing a developer"
           />
         </div>
       </div>
